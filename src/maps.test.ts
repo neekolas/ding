61eafa11a97bbe02892a9e74aafb1e3b2d7ae128
Nick Molnar <65710+neekolas@users.noreply.github.com>
2018-02-23 23:32:10 -0800

    Revert "Add missing test"
    This reverts commit b5ba789fe04d852c936a1bb897cadad10ada1f13.

--- src/maps.test.ts
+++
@@ -1,8 +0,0 @@
-import { lookupAddress } from './maps';
-
-it('Can find my building', async () => {
-    const results = await lookupAddress('4900 Cartier St, Vancouver');
-    expect(results.length).toBeGreaterThan(0);
-    const result = results[0];
-    expect(result.formatted_address).toContain('Vancouver');
-});
