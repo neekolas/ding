4e758ce5dadb0a2d93fcbcb8df40f473a66b7301
Nick Molnar <65710+neekolas@users.noreply.github.com>
2019-02-15 10:26:15 -0800

    Better logging

--- src/voice/logger.ts
+++ src/voice/logger.ts
@@ -3,6 +3,18 @@ export interface BuzzLogger {
 	log(...args): void;
 	error(...args): void;
 }
+
+const stringify = something => {
+	switch (typeof something) {
+		case 'string':
+			return something;
+		case 'number':
+			return something.toString();
+		default:
+			return JSON.stringify(something);
+	}
+};
+
 export function buzzLogger(buzz: Buzz) {
 	const params = {
 		callSid: buzz.nodeID,
@@ -11,10 +23,10 @@ export function buzzLogger(buzz: Buzz) {
 	};
 	return {
 		log(...args) {
-			console.log(JSON.stringify({ ...params, message: args.map(arg => JSON.stringify(arg)).join(' ') }));
+			console.log(JSON.stringify({ ...params, message: args.map(stringify).join(' ') }));
 		},
 		error(...args) {
-			console.error(JSON.stringify({ ...params, message: args.map(arg => JSON.stringify(arg)).join(' ') }));
+			console.error(JSON.stringify({ ...params, message: args.map(stringify).join(' ') }));
 		}
 	};
 }
