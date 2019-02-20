26673f58a3fbd4c78baa9f7e57be9c80fcc29bbd
Nick Molnar <65710+neekolas@users.noreply.github.com>
2019-02-20 15:49:46 -0800

    Add session

--- src/slack/index.ts
+++ src/slack/index.ts
@@ -1,7 +1,9 @@
 import { Strategy as SlackStrategy } from 'passport-slack';
 import passport from 'passport';
-import express from 'express';
-import { dbMiddleware } from '../db';
+import express, { Request } from 'express';
+import { dbMiddleware, DB } from '../db';
+import session from 'express-session';
+
 const { SLACK_CLIENT_ID, SLACK_CLIENT_SECRET } = process.env;
 const callbackURL = `https://manage.dingdong.buzz/slack/callback`;
 passport.use(
@@ -21,14 +23,31 @@ passport.use(
     )
 );
 
+export type SlackRequest = Request & {
+    db: DB;
+    session: any;
+};
+
 export default function() {
     const app = express();
     app.use(passport.initialize());
     app.use(require('body-parser').urlencoded({ extended: true }));
-
+    app.set('trust proxy', 1);
+    app.use(
+        session({
+            secret: 'dingdong is the best ever',
+            resave: false,
+            saveUninitialized: true,
+            cookie: { secure: true }
+        })
+    );
     // path to start the OAuth flow
     app.get(
         '/slack/login',
+        function(req: SlackRequest, res, next) {
+            const { query } = req;
+            req.session = query;
+        },
         passport.authorize('slack', {
             successRedirect: callbackURL
         })
@@ -38,7 +57,8 @@ export default function() {
     app.get(
         '/slack/callback',
         passport.authorize('slack', { failureRedirect: '/slack/login' }),
-        (req, res) => {
+        (req: SlackRequest, res) => {
+            console.log(req.session);
             console.log('USER', req.user);
             res.redirect('https://manage.dingdong.buzz');
         }
